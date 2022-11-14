775a8f1894cb019f43a50d74bde13ce3e66b1e14
masonleonhart <mason.leonhart@gmail.com>
2022-11-14 11:49:50 -0600

    Set Up templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 from model import db
 
@@ -7,4 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
+    return render_template("welcome.html")
+
+
+@app.route("/add_card", methods=["GET", "POST"])
+def add_card(index):
+    return db
+
+
+@app.route("/remove_card", methods=["GET", "POST"])
+def remove_card(index):
     return db
